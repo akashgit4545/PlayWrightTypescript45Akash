bc4ff304b7805ed6f1759529060274c882e59889
akashgit4545 <akkipatil002325@gmail.com>
2026-03-20 13:42:47 +0530

    feat: check merge

--- tests/orangehrm.spec.ts
+++ tests/orangehrm.spec.ts
@@ -29,6 +29,7 @@ test('Invalid Login to OrangeHRM', async ({ page }) => {
     const password: Locator = page.locator('input[name="password"]');
     const loginButton: Locator = page.locator('button[type="submit"]');
     const errorMessage: Locator = page.getByText('Invalid credentials');
+    
     await username.fill('Admin');
     await password.fill('admin1');
     await loginButton.click();
