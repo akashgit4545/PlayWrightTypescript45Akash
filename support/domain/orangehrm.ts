75aee62440e2c12de87f437b408d182c941392c9
akashgit4545 <akkipatil002325@gmail.com>
2026-03-21 17:21:05 +0530

    feat: add screen element validation to OrangeHRM interface and implementation

--- support/domain/orangehrm.ts
+++ support/domain/orangehrm.ts
@@ -22,4 +22,22 @@ export class OrangeHRM implements OrangeHRMInterface {
         await expect(this.page.locator(OrangeHRMLocators.errorMessage)).toBeVisible({ timeout: 10000 });
         await expect(this.page.locator(OrangeHRMLocators.errorMessage)).toHaveText(orangeHrmPageConstants.ERROR_MESSAGE);
     }
+
+    async validateScreenElements(): Promise<void> {
+        await expect(this.page.locator(OrangeHRMLocators.companyLogo)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.textLogin)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.textLogin)).toHaveText(orangeHrmPageConstants.TEXT_LOGIN);
+        await expect(this.page.locator(OrangeHRMLocators.orangeLogo)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.labelUsername)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.labelUsername)).toHaveText(orangeHrmPageConstants.USERNAME);
+        await expect(this.page.locator(OrangeHRMLocators.labelPassword)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.labelPassword)).toHaveText(orangeHrmPageConstants.PASSWORD);
+        await expect(this.page.locator(OrangeHRMLocators.linkForgotPassword)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.linkForgotPassword)).toHaveText(orangeHrmPageConstants.FORGOT_PASSOWRD);
+        await expect(this.page.locator(OrangeHRMLocators.copyright1).nth(0)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.copyright1).nth(0)).toHaveText(orangeHrmPageConstants.COPYRIGHT_ONE);
+        await expect(this.page.locator(OrangeHRMLocators.copyright1).nth(1)).toBeVisible();
+        await expect(this.page.locator(OrangeHRMLocators.copyright1).nth(1)).toHaveText(orangeHrmPageConstants.COPYRIGHT_TWO);
+        await expect(this.page.locator(OrangeHRMLocators.copyrightLink)).toHaveAttribute('href', OrangeHRMUrls.orangeHRMInc);
+    }
 }
--- support/interface/orangehrm-interface.ts
+++ support/interface/orangehrm-interface.ts
@@ -3,6 +3,7 @@ export interface OrangeHRMInterface {
     login(username: string, password: string): Promise<void>;
     verifyLoginSuccess(): Promise<void>;
     verifyLoginFailure(): Promise<void>;
+    validateScreenElements(): Promise<void>;
 }
 
 export const OrangeHRMLocators = {
@@ -10,13 +11,29 @@ export const OrangeHRMLocators = {
     passwordInput: 'input[name="password"]',
     loginButton: 'button[type="submit"]',
     errorMessage: 'text=Invalid credentials',
+    companyLogo: 'img[alt="company-branding"]',
+    textLogin: 'h5:has-text("Login")',
+    labelUsername: 'label:has-text("Username")',
+    labelPassword: 'label:has-text("Password")',
+    linkForgotPassword: 'text=Forgot your password?',
+    orangeLogo: 'div.orangehrm-login-logo',
+    copyright1: 'p.orangehrm-copyright',
+    copyrightLink: 'p.orangehrm-copyright a',
+
 }
 
 export enum OrangeHRMUrls {
     loginPage = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
     dashboardPage = 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',
+    orangeHRMInc = 'http://www.orangehrm.com'
 }
 
 export enum orangeHrmPageConstants {
-    ERROR_MESSAGE = 'Invalid credentials'
-}
+    ERROR_MESSAGE = 'Invalid credentials',
+    TEXT_LOGIN = 'Login',
+    USERNAME = 'Username',
+    PASSWORD = 'Password',
+    FORGOT_PASSOWRD = 'Forgot your password? ',
+    COPYRIGHT_ONE = 'OrangeHRM OS 5.8',
+    COPYRIGHT_TWO = '© 2005 - 2026 OrangeHRM, Inc. All rights reserved.'
+}   
